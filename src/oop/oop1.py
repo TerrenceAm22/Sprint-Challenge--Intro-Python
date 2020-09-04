c4ad988be8d882ea3358e9f0216be6676fa10745
TerrenceAm22 <tamalone87@gmail.com>
2020-09-04 13:05:19 -0400

    MVP

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,52 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    """
+    Base Class
+    """
+    pass
+
+
+class GroundVehicle(Vehicle):
+    """ 
+    Inheriting from Vechicle Class
+    """
+    pass
+
+class Car(GroundVehicle):
+    """
+    Inheriting from GroundVechicle
+    """
+    pass
+
+class Motorcycle(GroundVehicle):
+    """
+    Inheriting from Ground Vechicle
+    """
+    pass
+
+
+class FlightVehicle(Vehicle):
+    """
+    Inheriting from base class
+    since this is a form a vechicle
+    and so it would not share
+    attributes from the ground vechicles class
+    necessarily
+    """
+    pass
+
+class Starship(FlightVehicle):
+    """ 
+    Inheriting from Flight class
+    """
+    pass
+
+class Airplane(FlightVehicle):
+    """
+    Inheriting from flight class
+    """
+    pass
+
